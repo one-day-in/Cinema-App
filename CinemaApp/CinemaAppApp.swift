56400c81efa6248ae2b7d77969e6e822f587ea71
Artem <skabullartem@gmail.com>
2024-11-27 08:25:57 +0700

    Initial Commit

---
+++ CinemaApp/CinemaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaAppApp.swift
+//  CinemaApp
+//
+//  Created by Artem Kotoshchuk on 27.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct CinemaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CinemaApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CinemaApp
+//
+//  Created by Artem Kotoshchuk on 27.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
